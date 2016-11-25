ec32b399d15324bc94808cfe06ce8d06cb11bca0
Itokoyamato <dthuilli@student.42.fr>
2016-11-25 14:20:51 +0100

    Minor change

--- srcs/solver.c
+++ srcs/solver.c
@@ -89,7 +89,7 @@ t_map	*solve_fillit(t_list *tetris)
 
 	map_size = 2;
 	while (map_size * map_size < (int)(ft_lstcount(tetris) * 4))
-		map_size++;
+		++map_size;
 	map = buildmap(map_size);
 	while (!try_map(map, tetris))
 	{
