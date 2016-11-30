5607a22cd12aad12eec627bcd200f81779f0bcf2
Itokoyamato <dthuilli@student.42.fr>
2016-11-30 16:12:31 +0100

    Works fine, memory leaks to test

---
+++ libft/ft_lstdelone.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthuilli <dthuilli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/08 11:21:46 by dthuilli          #+#    #+#             */
+/*   Updated: 2016/11/08 11:30:41 by dthuilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
+{
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+}
---
+++ libft/ft_lstnew.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthuilli <dthuilli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/08 10:51:23 by dthuilli          #+#    #+#             */
+/*   Updated: 2016/11/09 13:53:02 by dthuilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*newlist;
+
+	newlist = (t_list*)malloc(sizeof(t_list));
+	if (newlist == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		newlist->content = NULL;
+		newlist->content_size = 0;
+	}
+	else
+	{
+		newlist->content = malloc(content_size);
+		if (newlist->content == NULL)
+			return (NULL);
+		ft_memcpy(newlist->content, content, content_size);
+		newlist->content_size = content_size;
+	}
+	newlist->next = NULL;
+	return (newlist);
+}
