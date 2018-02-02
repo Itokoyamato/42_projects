ec5e1c903c82015e14b1de794cf0c898fac5fb97
Itokoyamato <dthuilli@student.42.fr>
2018-02-02 18:59:04 +0100

    Changed scale of map generation Added parsing of turns Added per-move player with animations Added texture to rooms

--- bonus/js/visualizer.js
+++ bonus/js/visualizer.js
@@ -1,8 +1,18 @@
 var scene, camera, renderer, controls;
 var geometry, material, mesh;
-var rooms = [];
-var tunnels = [];
-var ants = [];
+
+var isRunning = false;
+var arr_rooms = [];
+var arr_rooms_obj = [];
+var arr_tunnels = [];
+var arr_ants = [];
+var arr_turns = [];
+var start_room;
+var end_room;
+var current_turn = -1;
+var ant_count = 0;
+var room_count = 0;
+var	turn_startTime;
 
 var instructions = document.getElementById( 'instructions' );
 var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
@@ -16,9 +26,45 @@ var prevTime = performance.now();
 var velocity = new THREE.Vector3();
 var direction = new THREE.Vector3();
 var raycaster = new THREE.Raycaster();
+
 var skybox;
+var texture_dirt;
+var texture_glow;
 var ant_object;
-var isRunning = false;
+var ant_material = new THREE.MeshLambertMaterial({ color: 0xFF0000, opacity: 1});
+
+function stop()
+{
+	console.log("stop");
+}
+
+function play()
+{
+	console.log("play");
+}
+
+function pause()
+{
+	console.log("pause");
+}
+
+function next_move()
+{
+	console.log("next_move");
+	current_turn++;
+	var turn = arr_turns[current_turn];
+	if (turn)
+	{
+		for (i in turn)
+		{
+			var move = turn[i];
+			move.ant.moving = true;
+			move.ant.dest = move.dest;
+			turn_startTime = performance.now();
+			move.ant.object.lookAt(move.ant.dest.object.position);
+		}
+	}
+}
 
 window.onload = function() {
 	var loader = new THREE.OBJLoader();
@@ -26,7 +72,11 @@ window.onload = function() {
 		function ( object ) {
 			object.traverse( function ( child ) {
 				if ( child instanceof THREE.Mesh )
-					child.material.color.setHex(0x151515);
+				{
+					child.material = ant_material;
+					child.geometry.applyMatrix(new THREE.Matrix4().makeRotationY(toRadians(-90)));
+					child.geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-0.2, 0.1, 0));
+				}
 			});
 			ant_object = object;
 		},
@@ -43,16 +93,21 @@ window.onload = function() {
 
 function reset_scene()
 {
-	for (i in rooms)
-		scene.remove(rooms[i].object);
-	for (i in tunnels)
-		scene.remove(tunnels[i].object);
-	for (i in ants)
-		scene.remove(ants[i].object);
+	for (i in arr_rooms)
+		scene.remove(arr_rooms[i].object);
+	for (i in arr_tunnels)
+		scene.remove(arr_tunnels[i].object);
+	for (i in arr_ants)
+		scene.remove(arr_ants[i].object);
 	document.getElementById("fileToLoad").value = "";
-	rooms = [];
-	tunnels = [];
-	ants = [];
+	arr_rooms = [];
+	arr_tunnels = [];
+	arr_ants = [];
+	arr_turns = [];
+	start_room = null;
+	end_room = null;
+	ant_count = 0;
+	current_turn = -1;
 	document.getElementById("result").innerHTML = "";
 	moveForward = false;
 	moveBackward = false;
@@ -63,7 +118,6 @@ function reset_scene()
 	direction = new THREE.Vector3();
 	controls = new THREE.PointerLockControls(camera);
 	controls.getObject().position.set(10, 7, 40);
-	// controls.getObject().position.set(2, 2, 0);
 	scene.add(controls.getObject());
 	isRunning = false;
 }
@@ -85,6 +139,7 @@ function loadFileAsText(element){
 	fileReader.readAsText(fileToLoad, "UTF-8");
 }
 
+var start_end = 0;
 function parse_data(data)
 {
 	var lines = data.split("\n");
@@ -92,26 +147,39 @@ function parse_data(data)
 	{
 		var l = lines[i];
 		if (i == 0)
-		{
-			// Ants processing
-		}
+			ant_count = parseInt(l);
 		else
 		{
 			if (l.includes("#") || l.includes("##"))
 			{
-
+				if (l.includes("##start"))
+					start_end = 1;
+				if (l.includes("##end"))
+					start_end = 2;
+			}
+			else if (l.includes("L"))
+			{
+				var moves = [];
+				var split = l.split(" ");
+				for (j in split)
+				{
+					var split2 = split[j].replace("L", "").split("-");
+					var result = arr_rooms.filter(function(obj) {return obj.name == split2[1];});
+					moves.push({ant: arr_ants[split2[0]-1], dest: result[0]});
+				}
+				arr_turns.push(moves);
 			}
 			else if (l.includes("-"))
 			{
 				var split = l.split("-");
 				var room1;
 				var room2;
-				for (var j = 0; j < rooms.length; j++)
+				for (var j = 0; j < arr_rooms.length; j++)
 				{
-					if (rooms[j].name == split[0])
-						room1 = rooms[j];
-					if (rooms[j].name == split[1])
-						room2 = rooms[j];
+					if (arr_rooms[j].name == split[0])
+						room1 = arr_rooms[j];
+					if (arr_rooms[j].name == split[1])
+						room2 = arr_rooms[j];
 				}
 				if (room1 && room2)
 					addTunnel(room1, room2);
@@ -119,7 +187,8 @@ function parse_data(data)
 			else
 			{
 				var split = l.split(" ");
-				addRoom(parseInt(split[1]), parseInt(split[2]), split[0]);
+				room_count++;
+				addRoom(parseInt(split[1])*5, parseInt(split[2])*5, split[0]);
 			}
 		}
 	}
@@ -170,22 +239,27 @@ function init() {
 	camera = new THREE.PerspectiveCamera(75, element.offsetWidth / element.offsetHeight, 1, 10000);
 
 	var light = new THREE.PointLight(0xffffff, 1, 100);
-	light.position.set(0, 10, 0);
+	light.position.set(10, 10, 10);
+	scene.add( light );
+
+	light = new THREE.PointLight(0xffffff, 1, 100);
+	light.position.set(-10, 10, 10);
 	scene.add( light );
 
 	var light = new THREE.AmbientLight(0x404040, 2);
 	scene.add( light );
 
-	var geometry = new THREE.SphereGeometry(100, 124, 124);
-	var texture = new THREE.TextureLoader().load("skybox.jpg");
-	var material = new THREE.MeshPhongMaterial({color: 0xffffff, map: texture})
+	var geometry = new THREE.SphereGeometry(1000, 32, 32);
+	texture_dirt = new THREE.TextureLoader().load("skybox.jpg");
+	var material = new THREE.MeshPhongMaterial({color: 0xffffff, map: texture_dirt})
 	skybox = new THREE.Mesh(geometry, material);
 	skybox.material.side = THREE.DoubleSide;
 	scene.add(skybox);
 
+	texture_glow = new THREE.TextureLoader().load("glow.png");
+
 	controls = new THREE.PointerLockControls(camera);
 	controls.getObject().position.set(10, 7, 40);
-	// controls.getObject().position.set(2, 2, 0);
 	scene.add(controls.getObject());
 
 	var onKeyDown = function ( event ) {
@@ -240,7 +314,8 @@ function init() {
 	element.appendChild(renderer.domElement);
 }
 
-delta_amount = 100.0;
+var delta_amount = 100.0;
+var last_raycast = performance.now();
 function animate()
 {
 	requestAnimationFrame(animate);
@@ -270,26 +345,51 @@ function animate()
 		controls.getObject().translateY( velocity.y * delta );
 		prevTime = time;
 
-		raycaster.set( camera.getWorldPosition(), camera.getWorldDirection() );
-		var intersects = raycaster.intersectObjects( scene.children );
-		var info = document.getElementById("info_content");
-		if (intersects[0])
+		if (time - last_raycast > 100)
 		{
-			if (skybox === intersects[0].object)
-				info.innerHTML = "";
-			for (i in rooms)
+			raycaster.set( camera.getWorldPosition(), camera.getWorldDirection() );
+			var intersects = raycaster.intersectObjects(arr_rooms_obj);
+			var info = document.getElementById("info_content");
+			if (intersects[0])
 			{
-				if (rooms[i].object === intersects[0].object)
-					info.innerHTML = "Room: " + rooms[i].name + "<br>" + "Ants: " + rooms[i].ants;
+				if (skybox === intersects[0].object)
+					info.innerHTML = "";
+				for (i in arr_rooms)
+				{
+					if (arr_rooms[i].object === intersects[0].object)
+						info.innerHTML = "Room: " + arr_rooms[i].name + "<br>" + "Ants: " + arr_rooms[i].ants;
+				}
+				for (i in arr_tunnels)
+				{
+					if (arr_tunnels[i].object === intersects[0].object)
+						info.innerHTML = "Tunnel: " + arr_tunnels[i].name + "<br>" + "Ants: " + arr_tunnels[i].ants;
+				}
 			}
-			for (i in tunnels)
+			else
+				info.innerHTML = "";
+			last_raycast = time;
+		}
+	}
+	for (i in arr_ants)
+	{
+		var ant = arr_ants[i];
+		if (ant.moving)
+		{
+			var tmp = new THREE.Vector3();
+			var	dest = ant.room.object.position.clone();
+			var progress = Math.min((performance.now() - turn_startTime) / 2000);
+
+			dest.z += 0.5;
+			dest.y -= 0.5;
+			tmp.copy(ant.dest.object.position).sub(ant.room.object.position).multiplyScalar(progress);
+			ant.object.position.copy(dest).add(tmp);
+			if (progress >= 1)
 			{
-				if (tunnels[i].object === intersects[0].object)
-					info.innerHTML = "Tunnel: " + tunnels[i].name + "<br>" + "Ants: " + tunnels[i].ants;
+				ant.moving = false;
+				ant.room = ant.dest;
+				ant.dest = null;
 			}
 		}
-		else
-			info.innerHTML = "";
 	}
 	renderer.render(scene, camera);
 }
@@ -302,10 +402,11 @@ function cylinderMesh(pointX, pointY, material) {
 		0, 0, 1, 0,
 		0, -1, 0, 0,
 		0, 0, 0, 1));
-	var geometry = new THREE.CylinderGeometry(0.1, 0.1, direction.length(), 8, 1);
-	var texture = new THREE.TextureLoader().load("skybox.jpg");
+	var geometry = new THREE.CylinderGeometry(1.5, 1.5, direction.length() - 5, 8, 1, true);
+	var texture = texture_dirt.clone();
+	texture.needsUpdate = true;
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set(2 * Math.PI * 0.1 / direction.length(), 2);
+	texture.repeat.set(20 * Math.PI * 0.1 / direction.length(), 1);
 	material.map = texture;
 	var mesh = new THREE.Mesh(geometry, material);
 	mesh.applyMatrix(orientation);
@@ -317,16 +418,32 @@ function cylinderMesh(pointX, pointY, material) {
 
 function addRoom(x, y, name, ants)
 {
-	var room = new THREE.BoxGeometry(1, 0.5, 1);
-	var material = new THREE.MeshLambertMaterial({color: 0xff0000});
+	var room = new THREE.SphereGeometry(3, 16, 16);
+	var material = new THREE.MeshLambertMaterial({transparent: true, opacity: 0.7});
 
 	var mesh = new THREE.Mesh(room, material);
-	var z = Math.floor(Math.random() * 10);
+	var z = Math.floor(Math.random() * 20);
 	mesh.position.set(x, z, y);
+	material.map = texture_dirt;
+	material.depthWrite = false;
 	scene.add(mesh);
 	room = {object: mesh, name: name, ants: ants};
-	rooms.push(room);
-	addAnt(x, z + 0.2, y + 0.2);
+	arr_rooms.push(room);
+	arr_rooms_obj.push(mesh);
+	if (start_end == 1)
+	{
+		start_room = room;
+		start_end = 0;
+		for (var j = 1; j <= ant_count; j++)
+		{
+			addAnt(x, z, y, room);
+		}
+	}
+	if (start_end == 2)
+	{
+		end_room = room;
+		start_end = 0;
+	}
 	return (room);
 }
 
@@ -335,24 +452,38 @@ function toRadians(angle)
 	return (angle * (Math.PI / 180));
 }
 
-function addAnt(x, y, z)
+function addAnt(x, y, z, room)
 {
 	var ant = ant_object.clone();
-	ant.scale.set(0.5,0.5,0.5);
-	ant.position.set(x, y, z);
+	ant.position.set(x - 0.6, y, z);
 	scene.add(ant);
-	var ant = {object: ant};
-	ants.push(ant);
+
+	var spriteMaterial = new THREE.SpriteMaterial( 
+	{ 
+		map: texture_glow,
+		color: 0xFF0000,
+		transparent: false,
+		blending: THREE.AdditiveBlending,
+		opacity: 0.1
+	});
+	var sprite = new THREE.Sprite(spriteMaterial);
+	sprite.scale.set(3, 3, 1.0);
+	// sprite.translateZ(-0.6);
+	// sprite.translateY(0.2);
+	// ant.add(sprite);
+
+	ant = {object: ant, room: room};
+	arr_ants.push(ant);
 }
 
 function addTunnel(room1, room2, ants)
 {
-	var tunnel = new THREE.CylinderGeometry( 0.1, 0.1, 10);
-	var material = new THREE.MeshLambertMaterial({color: 0xffffff});
+	var tunnel = new THREE.CylinderGeometry(0.1, 0.1, 10);
+	var material = new THREE.MeshLambertMaterial({color: 0xffffff, transparent: true, opacity: 0.7});
 
 	var mesh = new cylinderMesh(room1.object.position, room2.object.position, material);
 	scene.add(mesh);
 	tunnel = {object: mesh, name: room1.name + "-" + room2.name, ants: ants};
-	tunnels.push(tunnel);
+	arr_tunnels.push(tunnel);
 	return (tunnel);
 }
